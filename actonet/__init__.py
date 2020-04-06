8b947f204b6686db46212943826d1c2a688e35cb
Loïc Paulevé <loic.pauleve@ens-cachan.org>
2020-04-06 16:01:08 +0200

    use BooleanNetwork.auto_cast for simpler input

--- actonet/__init__.py
+++ actonet/__init__.py
@@ -1,9 +1,14 @@
 
 __version__ = "0.1"
 
+import sys
+
 from .iface import ActoNet
 
+from colomoto.minibn import BooleanNetwork
+
 def load(bn):
+    bn = BooleanNetwork.auto_cast(bn)
     return ActoNet(bn)
 
 
